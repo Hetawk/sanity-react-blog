c4ccadc17b31c43a1472d84360954cc1147d4028
ENOCH KWATEH DONGBO <enoch.dongbo@gmail.com>
2023-11-30 17:15:10 +0800

    Update commit

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
+
   projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2021-08-31',
@@ -10,6 +11,7 @@ export const client = sanityClient({
   ignoreBrowserTokenWarning: true,
 
 });
+
 
 const builder = imageUrlBuilder(client);
 
